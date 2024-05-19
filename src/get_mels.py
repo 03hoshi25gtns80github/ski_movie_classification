a9069d812a10b6e8b83508019a1e7af5a21964e0
03hoshi25gtns80git <03hoshi25gtns80@gmail.com>
2024-05-19 12:50:19 +0900

    対象の動画ファイル形式を増やした

--- src/get_mels.py
+++ src/get_mels.py
@@ -15,7 +15,7 @@ from PIL import Image
 def convert_mts_to_wav(input_folder, output_folder):
     # 入力フォルダ内のすべてのファイルを取得
     for filename in os.listdir(input_folder):
-        if filename.endswith(".MTS"):
+        if filename.endswith((".MTS", ".mp4")):
             input_path = os.path.join(input_folder, filename)
             output_filename = os.path.splitext(filename)[0] + ".wav"
             output_path = os.path.join(output_folder, output_filename)
